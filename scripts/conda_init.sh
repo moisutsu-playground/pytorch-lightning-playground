88f15c613bbff4f41dda4cfed4c18acae4fcb016
moisutsu <moisutsu@gmail.com>
2020-11-12 15:29:43 +0900

    Update conda_init.sh

--- scripts/conda_init.sh
+++ scripts/conda_init.sh
@@ -3,6 +3,6 @@ eval "$(conda shell.zsh hook)"
 conda activate pl-playground
 
 conda install -y pytorch cudatoolkit=10.1 -c pytorch
-conda install -y -c conda-forge tqdm jupyterlab
+conda install -y tqdm jupyterlab -c conda-forge
 pip install -U transformers pytorch-lightning ipywidgets fugashi ipadic
 jupyter labextension install @jupyter-widgets/jupyterlab-manager
