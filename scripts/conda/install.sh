5f1b07aa4861a8f0256ce10ba98f29ed98dfb08c
moisutsu <moisutsu@gmail.com>
2020-11-12 12:45:02 +0900

    Add env for conda

---
+++ scripts/conda/install.sh
@@ -0,0 +1,4 @@
+conda install pytorch cudatoolkit=10.1 -c pytorch
+conda install -y -c conda-forge tqdm jupyterlab
+pip install -U transformers pytorch-lightning ipywidgets
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
